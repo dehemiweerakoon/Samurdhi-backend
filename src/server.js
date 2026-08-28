78b220a34a2cf7c97fe5ddba3146f6d026637a26
dehemiweerakoon <dehemiweerakoon@gmail.com>
2026-08-28 23:28:32 +0530

    added

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ app.use(function(req, res, next) {
     const allowedOrigins = [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
+      '*'
     ];
     const requestOrigin = req.headers.origin;
 
